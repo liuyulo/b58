7f61aada8abe2e66b4f5c05b552725de6fce8c1b
Yulong Liu <yulo.liu@mail.utoronto.ca>
2023-04-07 13:44:24 -0400

    feat: update draw.py

--- draw.py
+++ draw.py
@@ -7,6 +7,20 @@ from argparse import ArgumentParser
 WIDTH = 512
 POINTER = 'v1'
 
+COLORS = {
+    0x2d260f: 0,
+    0x453917: 1,
+    0x5e4e1f: 2,
+    0x756127: 3,
+    0x8b742e: 4,
+    0xa98c38: 5,
+    0xba9b3e: 6,
+    0xd0ad45: 7,
+    0xe6c04c: 8,
+    0xfad053: 9,
+    0: 10,
+}
+
 
 def draw(im) -> Iterable[str]:
     w, _ = im.size
@@ -14,20 +28,20 @@ def draw(im) -> Iterable[str]:
     data = {}
     for i, color in enumerate(im.getdata()):
         if color[3] == 0:
-            color = (0, 0, 0)
-        color = '0x%02x%02x%02x' % color[:3]
+            continue
+        color = color[0] << 16 | color[1] << 8 | color[2]
+        color = min(COLORS, key=lambda x: abs(color - x))
         if color not in data:
             data[color] = []
         x, y = i % w, i // w
-        pos = x * 4 + y * WIDTH
-        data[color].append(pos)
+        data[color].append((x, y))
     for color, pos in data.items():
-        print(color)
-        register = '$0' if color == '0x000000' else '$t4'
-        if color != '0x000000':
-            yield f'li $t4 {color}'
-        for p in pos:
-            x, y = (p % WIDTH) // 4, p // WIDTH
+        register = '$0' if color == 0 else f'$t{COLORS[color]}'
+        # register = '$0' if color == 0 else f'$t4'
+        # if color != 0:
+        #     yield f'li $t4 0x{color:06x}'
+        for x, y in pos:
+            p = x * 4 + y * WIDTH
             yield f'sw {register} {p}(${POINTER}) # ({x}, {y})'
     yield 'jr $ra'
 
